c537d5a560a57eef2cdacdf1f83557cbee8b0b64
slady <csl.bunny.2207@gmail.com>
2022-10-30 20:27:32 +0100

    start and stop countdown

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,33 @@
 var playing = false;
 var score;
+var action;
+var timeremaining;
+
 // if we click on start/reset
 document.getElementById("startreset").onclick = function () {
   //    if we are playing
   if (playing == true) {
     location.reload(); //reload page
   } else {
-    //    if we are not playing
+    //   if we are not playing
 
-    // change mode to playing
+    //    change mode to playing
     playing = true;
 
     //      set score to 0
     score = 0;
     document.getElementById("scorevalue").innerHTML = score;
+
     //      show countdown box
-    document.getElementById("time").style.display = "block";
-    //      reduce time by 1s in loops
+    show("time");
+    timeremaining = 60;
+    document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+
     //      change button to reset
     document.getElementById("startreset").innerHTML = "Reset Game";
+
+    //     start countdown
+    startCountdown();
   }
 };
 
@@ -37,3 +46,34 @@ document.getElementById("startreset").onclick = function () {
 //        generate new q&a
 //      no
 //        show try again for 1s
+
+function startCountdown() {
+  action = setInterval(function () {
+    timeremaining -= 1;
+    document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+    if (timeremaining == 0) {
+      // game over
+      stopCountdown();
+
+      show("gameOver");
+      document.getElementById("gameOver").innerHTML =
+        "<p>Game over!</p><p>Your score is " + score + ".</p>";
+
+      hide("time");
+      hide("correct");
+      hide("wrong");
+      playing = false;
+    }
+  }, 1000);
+}
+
+function stopCountdown() {
+  clearInterval(action);
+}
+
+function hide(Id) {
+  document.getElementById(Id).style.display = "none";
+}
+function show(Id) {
+  document.getElementById(Id).style.display = "block";
+}
